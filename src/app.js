c8651516414956317767cd63ebc32e44f2ba05a6
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air.local>
2026-02-21 11:25:49 +0530

    Complete MVC Structure With Database Setup

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(express.json());
+app.use(cors());
+
+// Disable CSP in development
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+app.use(limiter);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+export default app;
